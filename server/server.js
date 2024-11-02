d3f32f322e6d1d2de6e460af7f1a7d773f4dc934
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2024-11-02 20:08:37 +0530

    Removed stashed changes from server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,3 @@
-<<<<<<< Updated upstream
-import express from 'express'
-import dotev from 'dotenv'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import connectDB from './config/db.js'
-
-// Load variable from .env file
-dotev.config()
-connectDB()
-=======
 import express from "express";
 import dotev from "dotenv";
 import cors from "cors";
@@ -21,7 +10,6 @@ dotev.config();
 
 //Connection
 connectDB();
->>>>>>> Stashed changes
 
 const app = express();
 
@@ -29,11 +17,8 @@ app.use(cors())
 app.use(bodyParser.json())
 
 // Routes
-<<<<<<< Updated upstream
-=======
 app.use("/profile", HistoryRoute);
 app.use("/user", authRouter)
->>>>>>> Stashed changes
 
 const PORT = process.env.PORT || 5000
 
