53878e2271d796e29ba9424f40c46f873811d381
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2024-11-17 08:36:27 +0530

    Added access for both local and server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import HistoryRoute from "./routes/HistoryRoute.js";
 import authRouter from "./routes/authRouter.js";
-import dataRouter from "./routes/dataRouter.js"
+import dataRouter from "./routes/dataRouter.js";
 
 // Load variable from .env file
 dotev.config();
@@ -13,23 +13,23 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: '*',
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
     methods: ["POST", "GET"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/history", HistoryRoute);
 app.use("/user", authRouter);
-app.use("/home", dataRouter)
+app.use("/home", dataRouter);
 app.get("/", (req, res) => {
-  res.status(200).json({msg: "The server is up and running"})
-})
+  res.status(200).json({ msg: "The server is up and running" });
+});
 
 //const PORT = process.env.PORT || 5000;
-
-
 
 app.listen(5000, () => {
   console.log(`Server running at port 5000`);
