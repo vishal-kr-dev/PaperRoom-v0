c3a653e2d53d1a50df6489ae8d874ec84e242606
Vishal Kumar <146015120+vishal-kr-dev@users.noreply.github.com>
2024-12-24 23:16:06 +0530

    Updated schemas

--- server/models/RoomSchema.js
+++ server/models/RoomSchema.js
@@ -1,9 +1,55 @@
 import mongoose from "mongoose";
 
+const GoalsSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  points: {
+    type: Number, 
+    required: true,
+  },
+  subtasks: [
+    {
+      description: {
+        type: String,
+        required: true,
+      },
+      point: {
+        type: Number,
+        default: 1,
+        immutable: true
+      },
+    },
+  ],
+  priority: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'medium'
+  },
+  dueDate:{
+    type: Date,
+    default: null    
+  }
+});
+
 const RoomSchema = new mongoose.Schema({
-  roomId: { type: String, required: true, unique: true },
-  users: [{ type: mongoose.Schema.Types.String, ref: "Users" }],
-  lastActive: { type: Date, default: Date.now },
+  roomId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  users: [
+    {
+      type: mongoose.Schema.Types.String,
+      ref: "Users",
+    },
+  ],
+  lastActive: {
+    type: Date,
+    default: Date.now,
+  },
+  tasks: [GoalsSchema]
 });
 
 const RoomModel = mongoose.model("Room", RoomSchema);
--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,37 +1,5 @@
 import mongoose from "mongoose";
 
-const GoalsSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
-  },
-  // type: {
-  //   type: String,
-  //   required: true
-  // },
-  deadline: {
-    type: Date,
-    required: false,
-  },
-  subtasks: [
-    {
-      description: {
-        type: String,
-        required: true,
-      },
-      isCompleted: {
-        type: Boolean,
-        default: false,
-      },
-    },
-  ],
-  isCompleted: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-});
-
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -46,7 +14,18 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  goals: [GoalsSchema],
+  monthlyPoints: {
+    type: Number,
+    default: 0,
+  },
+  streak: {
+    type: Number,
+    default: 0,
+  },
+  lastActiveDate: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const UserModel = mongoose.model("User", UserSchema);
